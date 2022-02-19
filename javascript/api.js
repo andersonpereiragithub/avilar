9f07a1972c06978ae5393e1af363a686b9d19d0d
Anderson Pereira <anderson.pereira@viannasempre.com.br>
2022-02-19 08:47:41 -0300

    Limpeza do código

--- javascript/api.js
+++ javascript/api.js
@@ -1,29 +1,13 @@
 
 function salvarImovel() {
-    console.log('Chamou!')
     var endereco = document.getElementById('id_end').value;
-    console.log(endereco);
-    
     var num = document.getElementById('num').value;
-    console.log(num);
-
     var aluguel = document.getElementById('id_aluguel').value;
-    console.log(aluguel);
-
     var iptu = document.getElementById('id_iptu').value;
-    console.log(iptu);
-
     var cond = document.getElementById('id_cond').value;
-    console.log(cond);
-
     var tipo = document.getElementById('id_tipo').value;
-    console.log(tipo);
-
     var desc = document.getElementById('txtarea').value;
-    console.log(desc);
-
     var status = document.getElementById('id_status').value;
-    console.log(status);
     
     var data = JSON.stringify({
         "endereco": endereco + ' ' + num,
@@ -36,18 +20,18 @@ function salvarImovel() {
     });
     
     var xhr = new XMLHttpRequest();
-    xhr.withCredentials = true;
+    //xhr.withCredentials = true;
 
-    xhr.addEventListener("readystatechange", function () {
+    /*xhr.addEventListener("readystatechange", function () {
         if (this.readyState === 4) {
             console.log(this.responseText);
         }
-    });
+    });*/
     
     xhr.open("POST", "http://localhost:8080/anuncios");
     xhr.setRequestHeader("content-type", "application/json");
-    xhr.setRequestHeader("cache-control", "no-cache");
-    xhr.setRequestHeader("postman-token", "133ab179-dd4a-6f8e-2b6c-dae7d9802770");
+    //xhr.setRequestHeader("cache-control", "no-cache");
+    //xhr.setRequestHeader("postman-token", "133ab179-dd4a-6f8e-2b6c-dae7d9802770");
     
     xhr.send(data);
 }
@@ -56,7 +40,7 @@ function listarImoveis() {
     var data = null;
 
     var xhr = new XMLHttpRequest();
-    xhr.withCredentials = true;
+    //xhr.withCredentials = true;
 
     xhr.addEventListener("readystatechange", function () {
         if (this.readyState === 4) {
@@ -81,11 +65,10 @@ function listarImoveis() {
                 td_aluguel.innerText = anuncios[i].aluguel.toFixed(2).replace('.', ',');
                 td_iptu.innerText = anuncios[i].iptu.toFixed(2).replace('.', ',');
                 td_cond.innerText = anuncios[i].condominio.toFixed(2).replace('.', ',');
+                tr_desc.innerText = anuncios[i].descricao;
                 
                 let somaTotal = anuncios[i].aluguel + anuncios[i].iptu + anuncios[i].condominio;
-                
                 td_total.innerText = somaTotal.toFixed(2).replace('.', ',');
-                tr_desc.innerText = anuncios[i].descricao;
                 
                 tr.setAttribute('id', `${anuncios[i].id}`);
                 tr.setAttribute('class', 'dados');
@@ -105,8 +88,8 @@ function listarImoveis() {
 
     xhr.open("GET", "http://localhost:8080/anuncios");
     xhr.setRequestHeader("content-type", "application/json");
-    xhr.setRequestHeader("cache-control", "no-cache");
-    xhr.setRequestHeader("postman-token", "a692c64d-ad47-e5f9-f9da-af82f6aa1d8a");
+    //xhr.setRequestHeader("cache-control", "no-cache");
+    //xhr.setRequestHeader("postman-token", "a692c64d-ad47-e5f9-f9da-af82f6aa1d8a");
 
     xhr.send(data);
 }
